4a69e1372c7d83fcf9448daa57d9d9c828c6f9a1
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2026-01-28 13:29:38 +0530

    Created basic REST API structure

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,15 @@
 import express from "express";
 
+import authRoutes from "./routes/auth.routes";
+
 const app = express();
 const PORT = 3000;
 
-app.use('/',(req,res)=>{
+app.use("/",(req,res)=>{
     res.send("This is chatscure application");
 })
+
+app.use("/api/auth",authRoutes);
 
 app.listen(PORT,()=>{
     console.log("Server running on port "+PORT);
