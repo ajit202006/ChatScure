c6e6cada2f459b67f32617f9cc1a722216cb1f6c
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2026-02-17 14:40:17 +0530

    Added function to get messages in the chat

--- backend/src/routes/message.routes.ts
+++ backend/src/routes/message.routes.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { getUsersForSidebar } from "../controllers/message.controller";
+import { getUsersForSidebar, getMessages } from "../controllers/message.controller";
 import { protectRoute } from "../middlewares/auth.middleware";
 
 const router = express.Router()
 
 router.get("/users", protectRoute, getUsersForSidebar);
+
+router.get("/:id", protectRoute, getMessages);
 
 export default router;
