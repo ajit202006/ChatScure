2f2547c8c10e4e8d346bdb3eb12296f7b1525d75
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2026-02-17 14:25:16 +0530

    Added message route in the app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import { ObjectId } from "mongoose";
 
 import authRoutes from "./routes/auth.routes";
+import messageRoutes from "./routes/message.routes";
 import connectDB from "./lib/db";
 
 declare global {
@@ -23,6 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.use("/", (req, res) => {
     res.send("This is chatscure application");
