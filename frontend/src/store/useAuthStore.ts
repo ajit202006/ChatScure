4388324f32df00e2f1e82b15c7b4954797f0d923
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2026-02-18 12:31:59 +0530

    Added functions to check authentication and signup

--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -12,7 +12,30 @@ const useAuthStore: any = create((set) => ({
 
     isCheckingAuth: true,
 
-    
+    checkAuth: async () => {
+        try {
+            const res = await axiosInstance.get("/auth/check");
+            set({ authUser: res.data });
+        } catch (error) {
+            console.log("Error in checkAuth:", error);
+            set({ authUser: null });
+        } finally {
+            set({ isCheckingAuth: false });
+        }
+    },
+
+    signUp: async (data: { fullName: string, email: string, password: string }) => {
+        set({ isSigningUp: true });
+        try {
+            const res = await axiosInstance.post("/auth/signup", data);
+            set({ authUser: res.data })
+            toast.success("Account created successfully");
+        } catch (error: any) {
+            toast.error(error.message);
+        } finally {
+            set({ isSigningUp: false });
+        }
+    }
 }));
 
 export { useAuthStore }
