ee2236c7c424af617d197b58ca81b18983e72cc8
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2026-02-18 12:29:47 +0530

    Set up routers and routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,47 @@
+import { useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import SettingsPage from "./pages/SettingsPage";
+
+import { Loader2 } from "lucide-react";
+import { Toaster } from "react-hot-toast";
+import Navbar from "./components/Navbar";
+
+import { useAuthStore } from "./store/useAuthStore";
+
+
 function App() {
-  return (
-    <>
-      <div>
-        <h1>This is a react app</h1>
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader2 className="size-20 animate-spin" />
       </div>
-    </>
+    )
+  }
+  return (
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to={"/login"} />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to={"/"} />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to={"/login"} />} />
+      </Routes>
+
+      <Toaster />
+    </div>
   )
 }
 
