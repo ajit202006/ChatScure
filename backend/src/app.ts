47415063af0c36c35d76e8f1c83d83bc3081fa45
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2026-02-20 11:23:20 +0530

    Added cors package to solve CORS error

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import { ObjectId } from "mongoose";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.routes";
 import messageRoutes from "./routes/message.routes";
@@ -20,8 +20,12 @@ const app = express();
 const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI;
 
-app.use(express.json());
+app.use(express.json({ limit:'10mb'}));
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
